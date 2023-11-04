378e83ad0d62ecb5e57cba86684a7d24433ef6d8
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2023-11-04 14:26:12 +0530

    Time: 0 ms (100%), Space: 41.5 MB (23.13%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,23 +8,21 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution {   // Without Extra Space
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode a = new ListNode(100);
+        ListNode temp = a;
         ListNode temp1 = list1;
         ListNode temp2 = list2;
-        ListNode head = new ListNode(100);
-        ListNode temp = head;
-        
+
         while(temp1 != null && temp2 != null){
             if(temp1.val < temp2.val){
-                ListNode a = new ListNode(temp1.val);
-                temp.next = a;
-                temp = a;
+                temp.next = temp1;
+                temp = temp1;
                 temp1 = temp1.next;
             }else {
-                ListNode a = new ListNode(temp2.val);
-                temp.next = a;
-                temp = a;
+                temp.next = temp2;
+                temp = temp2;
                 temp2 = temp2.next;
             }
         }
@@ -33,6 +31,39 @@ class Solution {
         }else{
             temp.next = temp1;
         }
-        return head.next;
+        return a.next;
     }
 }
+
+
+
+//    Using Extra Space 
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode temp1 = list1;
+//         ListNode temp2 = list2;
+//         ListNode head = new ListNode(100);
+//         ListNode temp = head;
+        
+//         while(temp1 != null && temp2 != null){
+//             if(temp1.val < temp2.val){
+//                 ListNode a = new ListNode(temp1.val);
+//                 temp.next = a;
+//                 temp = a;
+//                 temp1 = temp1.next;
+//             }else {
+//                 ListNode a = new ListNode(temp2.val);
+//                 temp.next = a;
+//                 temp = a;
+//                 temp2 = temp2.next;
+//             }
+//         }
+//         if(temp1 == null){
+//             temp.next = temp2;
+//         }else{
+//             temp.next = temp1;
+//         }
+//         return head.next;
+//     }
+// }
