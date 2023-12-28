339b0541df26fd907e43ce1ab5a3ab7545b42fe6
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2023-12-28 08:09:10 +0530

    Time: 2 ms (44.65%), Space: 43.9 MB (52.12%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        int index = 0;
+        solve(nums,ans, index);
+        return ans;
+    }
+    public static void solve(int[] nums, List<List<Integer>> ans, int index){
+        if(index >= nums.length){
+            ans.add(convertIntArrayToList(nums));
+            return;
+        }
+        for(int j = index; j < nums.length; j++){
+            int temp = nums[index];
+            nums[index] = nums[j];
+            nums[j] = temp;
+
+            solve(nums, ans, index+1);
+
+            temp = nums[index];
+            nums[index] = nums[j];
+            nums[j] = temp;
+        }
+    }
+    private static List<Integer> convertIntArrayToList(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : array) {
+            list.add(num);
+        }
+        return list;
+    }
+}
