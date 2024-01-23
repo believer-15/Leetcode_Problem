45adf058747d3216fd86bbf1f5b22b78006711bf
Ujjwal Sinha <113638229+believer-15@users.noreply.github.com>
2024-01-23 23:08:18 +0530

    Time: 1 ms (100.00%), Space: 45 MB (7.09%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
@@ -1,13 +1,31 @@
+// class Solution {
+//     public boolean isAcronym(List<String> words, String s) {
+//         String res="";
+//         for(int i=0;i<words.size();i++){
+//             String g=words.get(i);
+//             res+=g.charAt(0);
+//         }
+//         if(res.equals(s)){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean isAcronym(List<String> words, String s) {
-        String res="";
-        for(int i=0;i<words.size();i++){
-            String g=words.get(i);
-            res+=g.charAt(0);
+        if (s.length() != words.size()) {
+            return false;
         }
-        if(res.equals(s)){
+        int cnt = 0;
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).charAt(0) == s.charAt(i))
+                cnt++;
+        }
+        if (cnt == s.length()) {
             return true;
         }
+
         return false;
     }
 }
